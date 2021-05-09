8307223633b6e30ef403b0fc13d9b52fade01db1
andrey_mardash <andrey.mardash@gmail.com>
2021-05-09 06:38:10 +0300

    The first file in the tree App.js

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+import {PostsList} from './features/posts/PostsList'
+import { AddPostForm } from './features/posts/AddPostForm'
+import { SinglePostPage } from './features/posts/SinglePostPage'
+
+import { Navbar } from './app/Navbar'
+import { EditPostForm } from './features/posts/EditPostForm'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <React.Fragment>
+                <AddPostForm />
+                <PostsList />
+              </React.Fragment>
+            )}
+          />
+          <Route exact path="/editPost/:postId" component={EditPostForm} />
+          <Route exact path='/posts/:postId' component={SinglePostPage} />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default App
